2d76d7e6accabdb296a0db43cd14d8970c7b8fc0
Amen Khaled <Aminkhaled@users.noreply.github.com>
2020-07-29 23:34:01 +0200

    i finished navbar and slick sliders

---
+++ vendor/js/script.js
@@ -0,0 +1,54 @@
+// interval is in milliseconds. 1000 = 1 second -> so 1000 * 10 = 10 seconds
+
+$(document).ready(function() {
+    $('.carousel').carousel({
+        interval: 500 * 10
+    });
+
+    setTimeout(function(){
+        $('.loader').css('display', 'none');
+    }, 4000);
+    $(window).scroll(function () {
+        console.log($(window).scrollTop())
+        if ($(window).scrollTop() > 280) {
+            $('.navbar').addClass('navbar-fixed');
+            $('.navbar-brand').addClass('fixed-brand');
+            $('.nav-nav-light').css('background-color','rgba(1,2,3,0.58)');
+
+        }
+        if ($(window).scrollTop() < 281) {
+            $('.navbar').removeClass('navbar-fixed');
+            $('.navbar-brand').removeClass('fixed-brand');
+
+        }
+    });
+
+
+    $('.mainSlider').slick({
+        slidesToShow: 6,
+        slidesToScroll: 1,
+        autoplay: true,
+        width: '300px',
+        autoplaySpeed: 1000,
+        responsive: [
+            {
+                breakpoint: 768,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 3
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
